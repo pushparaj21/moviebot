4ad5ea34c238dab10efef7264dd85fd0f6640ba3
pushparaj21 <singhpushparaj31@gmail.com>
2025-06-26 18:43:20 +0530

    Update project dependencies and remove unused components
    - Added `nativewind`, `prettier-plugin-tailwindcss`, and `tailwindcss` to dependencies.
    - Updated `react-native-safe-area-context` version to a compatible range.
    - Removed unused components and screens, including the NotFound screen and tab layout.
    - Simplified the root layout by removing font loading and theme provider.
    - Updated TypeScript configuration to include `nativewind-env.d.ts`.
    - Cleaned up the project structure by deleting unnecessary files and directories.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className=" text-left text-5xl text-primary">Pushparaj singh</Text>
+    </View>
+  );
+}
